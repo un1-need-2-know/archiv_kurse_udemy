ea979c5e5c31d52d698d299d23f10f809e7148d1
Falk Rund <Un1matr1x@users.noreply.github.com>
2021-01-31 10:27:17 +0100

    [A4/17] Add stuff to the Database

--- src/Controller/ArtikelController.php
+++ src/Controller/ArtikelController.php
@@ -2,17 +2,29 @@
 
 namespace App\Controller;
 
+use App\Entity\Artikel;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArtikelController extends AbstractController
 {
     #[Route('/artikel', name: 'artikel')]
-    public function index(): Response
+    public function index(Request $request)
     {
-        return $this->render('artikel/index.html.twig', [
+        $artikel = new Artikel();
+        $artikel->setTitel('Unser erster Artikel');
+
+        $emt = $this->getDoctrine()->getManager();
+        $emt->persist($artikel);
+        $emt->flush();
+
+        return new Response("Artikel wurde angelegt");
+
+    /*   return $this->render('artikel/index.html.twig', [
             'controller_name' => 'ArtikelController',
         ]);
+    */
     }
 }
