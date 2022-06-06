571207196c091ba9e6734d5b70ae95237391915c
Falk Rund <Un1matr1x@users.noreply.github.com>
2022-06-06 07:17:09 +0200

    [issues/239] add the first testcase by requiring the methode-php file and run the function

--- tests/FunctionTest.php
+++ tests/FunctionTest.php
@@ -4,5 +4,10 @@ use PHPUnit\Framework\TestCase;
 
 class FunctionTest extends TestCase
 {
+    public function testAdditionReturnsCorrectResult()
+    {
+        require 'functions.php';
 
+        $this->assertEquals(3, addition(1, 2));
+    }
 }
