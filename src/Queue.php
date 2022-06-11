22cb5252c07f755920731c0230c9288ce29a0ee7
Falk Rund <Un1matr1x@users.noreply.github.com>
2022-06-11 15:35:30 +0200

    [issues/263] add a max for queue items and throw an exeption when limit is reached

--- src/Queue.php
+++ src/Queue.php
@@ -2,10 +2,17 @@
 
 class Queue
 {
+    public const MAX_ITEMS = 5;
+
     protected $items = [];
 
     public function push($item)
     {
+        if ($this->getCount() == static::MAX_ITEMS) {
+
+            throw new QueueExeption("Queue is full");
+        }
+
         $this->items[] = $item;
     }
 
---
+++ src/QueueExeption.php
@@ -0,0 +1,6 @@
+<?php
+
+class QueueExeption extends Exception
+{
+
+}
