ebf0af29445525ae97864a2d5d2ae9dff4e4a37b
Falk Rund <Un1matr1x@users.noreply.github.com>
2022-06-11 15:36:28 +0200

    [issues/263] add testMaxNumberOfItemsCanBeAdded to check if the max number of items can be added

--- tests/QueueTest.php
+++ tests/QueueTest.php
@@ -54,4 +54,13 @@ class QueueTest extends TestCase
 
         $this->assertEquals('penis', static::$queue->pop());
     }
+
+    public function testMaxNumberOfItemsCanBeAdded()
+    {
+        for ($i = 0; $i < Queue::MAX_ITEMS; $i++) {
+            static::$queue->push($i);
+        }
+        $this->assertEquals(Queue::MAX_ITEMS, static::$queue->getCount());
+
+    }
 }
