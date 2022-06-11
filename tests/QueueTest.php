adb1a42d751ed11de6810141175d4a68ad626960
Falk Rund <Un1matr1x@users.noreply.github.com>
2022-06-11 09:54:06 +0200

    [issues/258] add intial test for an empty start

--- tests/QueueTest.php
+++ tests/QueueTest.php
@@ -4,5 +4,10 @@ use PHPUnit\Framework\TestCase;
 
 class QueueTest extends TestCase
 {
-    
+    public function testNewQueueIsEmpty()
+    {
+        $queue = new Queue;
+
+        $this->assertEquals(0, $queue->getCount());
+    }
 }
