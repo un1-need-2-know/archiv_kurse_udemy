7fcb73b05599576c3fbfcb7096c8e5612225de54
Falk Rund <Un1matr1x@users.noreply.github.com>
2022-06-15 12:54:44 +0200

    [issues/303] add a function in User class that depends on the mailer class

--- src/User.php
+++ src/User.php
@@ -6,8 +6,17 @@ class User
 
     public $name;
 
+    public $email;
+
     public function getFullName()
     {
         return trim($this->first_name.' '.$this->name);
+    }
+
+    public function notify($message)
+    {
+        $mailer = new Mailer;
+
+        return $mailer->sendMessage($this->email, $message);
     }
 }
