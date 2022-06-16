46315983b54497ecbfc2a83eb7e72a4fa69e3235
Falk Rund <Un1matr1x@users.noreply.github.com>
2022-06-16 15:17:54 +0200

    [issues/301] throw an exception, when email is empty

--- src/Mailer.php
+++ src/Mailer.php
@@ -4,6 +4,11 @@ class Mailer
 {
     public function sendMessage($email, $message)
     {
+        if (empty($email))
+        {
+            throw new Exception();
+        }
+
         sleep(3);
 
         echo "send '$message' to '$email'";
