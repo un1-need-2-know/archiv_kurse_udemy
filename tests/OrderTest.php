66cba0601dcf04b000edb0ecb73add919e34264a
Falk Rund <Un1matr1x@users.noreply.github.com>
2022-06-17 11:26:19 +0200

    [issues/298] add tearDown to use Mockery

--- tests/OrderTest.php
+++ tests/OrderTest.php
@@ -4,6 +4,11 @@ use PHPUnit\Framework\TestCase;
 
 class OrderTest extends TestCase
 {
+    public function tearDown(): void
+    {
+        Mockery::close();
+    }
+
     public function testOrderIsProcessed()
     {
         $gateway = $this->getMockBuilder('PaymentGateway')
