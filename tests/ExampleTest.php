9f0cad0fe4676edd732c10ac895f0728be3e87dd
Falk Rund <Un1matr1x@users.noreply.github.com>
2022-06-17 11:10:03 +0200

    [issues/299] add tearDown to integrad mockery with phpUnit

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -4,6 +4,11 @@ use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
 {
+    public function tearDown(): void
+    {
+        Mockery::close();
+    }
+
     public function testAddingOnePlusTwoResultIsThree()
     {
         $this->assertEquals(3, 1 + 2);
