e93ea3ec2b07dd02d4bdbed3e40877c4cc75af20
Falk Rund <Un1matr1x@users.noreply.github.com>
2022-06-18 14:51:26 +0200

    [issues/294] add first test for an default empty title, this test fails with an error, we need to fix by creating the class we want to test

---
+++ tests/ArticleTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class ArticleTest extends TestCase
+{
+    public function testTitleIsEmptyByDefault()
+    {
+        $article = new App\Article;
+    }
+}
