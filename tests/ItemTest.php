9b52944008da97a2118cc7efff4ab762ce54b1f1
Falk Rund <Un1matr1x@users.noreply.github.com>
2022-06-24 19:04:49 +0200

    [issues/288] add first test for the public methode

--- tests/ItemTest.php
+++ tests/ItemTest.php
@@ -4,4 +4,10 @@ use PHPUnit\Framework\TestCase;
 
 class ItemTest extends TestCase
 {
+    public function testDescriptionIsNotEmpty()
+    {
+        $item = new Item;
+
+        $this->assertNotEmpty($item->getDescription());
+    }
 }
