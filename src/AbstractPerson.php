f961bc8357e466c1f059109895b2d866a24afbd0
Falk Rund <Un1matr1x@users.noreply.github.com>
2022-06-26 14:42:25 +0200

    [issues/283] add abstract function for later testing

---
+++ src/AbstractPerson.php
@@ -0,0 +1,18 @@
+<?php
+
+abstract class AbstractPerson
+{
+    protected $surename;
+
+    public function __construct(string $surename)
+    {
+        $this->surename = $surename;
+    }
+
+    abstract protected function getTitle();
+
+    public function getNameAndTitle()
+    {
+        return $this->getTitle() . ' ' . $this->surename;
+    }
+}
