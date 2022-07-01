5585201fa4b4d287503aa146316d7d11647f44b4
Falk Rund <Un1matr1x@users.noreply.github.com>
2022-07-01 15:23:36 +0200

    [issues/282] add basic tests for the static methode

---
+++ tests/MailerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use phpDocumentor\Reflection\DocBlock\Tags\InvalidTag;
+use PHPUnit\Framework\TestCase;
+
+class MailerTest extends TestCase
+{
+    public function testSendMessageReturnsTrue()
+    {
+        $this->assertTrue(Mailer::send('penis@pump.it.up', 'Boobs'));
+    }
+
+    public function testInvalidArgumentExceptionIfMailIsEmpty()
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        Mailer::send('','Boobs');
+    }
+}
