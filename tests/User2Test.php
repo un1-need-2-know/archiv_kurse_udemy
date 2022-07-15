a35c0f393a5dc8a148e8cbef22b9d22afe1d52de
Falk Rund <Un1matr1x@users.noreply.github.com>
2022-07-15 17:17:38 +0200

    [issues/281] make the Callable injectable

--- tests/User2Test.php
+++ tests/User2Test.php
@@ -11,6 +11,7 @@ class User2Test extends TestCase
         $mailer = $this->createMock(Mailer::class);
 
         $user->setMailer($mailer);
+        $user->setMailerCallable([Mailer::class, 'send']);
 
         $this->assertTrue($user->notify('Boobs'));
     }
